7dcfe5da827edf8b50634281221ebb032784cb37
Ronaldo Reis <ronaldocreis7@gmail.com>
2023-08-30 19:35:41 -0300

    formBuilder

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -15,9 +16,21 @@ export class CriarPensamentoComponent implements OnInit {
     modelo: 'modelo1',
   };
 
-  constructor(private service: PensamentoService, private router: Router) {}
+  formulario!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulario reativo'],
+      autoria: [],
+      modelo: ['modelo1'],
+    });
+  }
 
   criarPensamento() {
     this.service.criar(this.pensamento).subscribe((pensamento) => {
