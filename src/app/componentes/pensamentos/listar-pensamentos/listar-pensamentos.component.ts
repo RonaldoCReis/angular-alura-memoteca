617e3babaa334690a3296b9a40d5039675baeae2
Ronaldo Reis <ronaldocreis7@gmail.com>
2023-09-07 13:13:22 -0300

    load more button

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -9,13 +9,20 @@ import { PensamentoService } from '../pensamento.service';
 })
 export class ListarPensamentosComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
-
+  haMaisPensamentos = true;
   paginaAtual = 1;
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe((pensamentos) => {
       this.listaPensamentos = pensamentos;
+    });
+  }
+
+  carregarMaisPensamentos(): void {
+    this.service.listar(++this.paginaAtual).subscribe((novosPensamentos) => {
+      this.listaPensamentos.push(...novosPensamentos);
+      if (!novosPensamentos.length) this.haMaisPensamentos = false;
     });
   }
 }
